8a3dd8a807b317c62774c6641d5582831c96cc3e
kincaid <krooskincaid314@gmail.com>
2023-08-19 10:44:23 +0300

    Made changes to the navbar toggle button

--- portfolio-app/src/Components/Hocs/Navbar.jsx
+++ portfolio-app/src/Components/Hocs/Navbar.jsx
@@ -2,28 +2,20 @@ import{Link,NavLink} from 'react-router-dom';
 import { useState } from 'react';
 import '../Styles/Navbar.css';
 import SettingsPhoneIcon from '@mui/icons-material/SettingsPhone';
+import { Modal } from '@mui/material';
 
 const Navbar = () => {
 
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [mouseOver,setMouseOver] = useState(false);
-  const[sectionClick, setSectionClick] = useState(false)
   
-
-  const mouseOverHandler = () => {
-    setMouseOver(true);
+  const openToggle = () => {
+    setIsNavOpen(true);
   };
-  const mouseOutHandler = () => {
-    setMouseOver(false)
-  }
-  //clicks 
-const handleSectionClick = () => [
-  setSectionClick(true)
-]
 
-  const handleToggleClick = () => {
-    setIsNavOpen(!isNavOpen);
+  const closeToggle = () => {
+    setIsNavOpen(false);
   };
+
 
   return (
 
@@ -35,39 +27,71 @@ const handleSectionClick = () => [
         </Link>
             <button className="navbar-toggler" 
                 type="button"
-                onClick={handleToggleClick}
+                onClick={openToggle}
                 data-bs-toggle="collapse" 
                 aria-expanded={isNavOpen} 
                 aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
              </button>
 
-    <div className={`collapse navbar-collapse${isNavOpen ? ' show' : ''}`} >
-        <ul className={`navbar-nav ms-auto mb-2 mb-lg-0 ${mouseOver ? 'decoration' : ''}`}
-                      onMouseOver={mouseOverHandler}
-                      onMouseOut={mouseOutHandler}>
+      <div className="collapse navbar-collapse" >
+        <ul className='navbar-nav ms-auto mb-2 mb-lg-0' >
             <li className= "nav-item .text-warning-emphasis">
-               <NavLink className="nav-link home-link  text-light" aria-current="page" to="/">Home</NavLink>
-            </li>
-            <li className="nav-item" onClick={handleSectionClick} style={{ color: sectionClick ? 'blue' : 'inherit' }}>
-                        <NavLink className="nav-link text-light" to="/about" >About</NavLink>
+               <NavLink className="nav-link text-hover" aria-current="page" to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link text-light text-capitalize" to="/projects">Projects</NavLink>
+                        <NavLink className="nav-link text-hover" to="/about" >About</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/resume" >Resume</NavLink>
+              <NavLink className="nav-link text-hover" to="/projects">Projects</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link text-light " to="/contact">Contact</NavLink>
+              <NavLink className="nav-link text-hover" to="/resume" >Resume</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-hover" to="/contact">Contact</NavLink>
             </li>
           </ul> 
           <p className="styled-contact">  <SettingsPhoneIcon className='call-settings' />  +254 701 218 138</p>
         </div>
-               
-        
-      </div>
+       
+
+
+            
+    <Modal open={isNavOpen} onClose={closeToggle}>
+      <div className="modal-container">   
+        <div className="modal-content">
+           <ul className={`navbar-nav ms-auto mb-2 mb-lg-0 `}>
+            <li className= "nav-item .text-warning-emphasis">
+               <NavLink className="nav-link home-link  " aria-current="page" to="/" onClick={closeToggle}>Home</NavLink>
+            </li>
+            
+            <li className="nav-item">
+                        <NavLink className="nav-link " to="/about" onClick={closeToggle}>About</NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link " to="/projects" onClick={closeToggle}>Projects</NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link " to="/resume" onClick={closeToggle}>Resume</NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link " to="/contact" onClick={closeToggle}>Contact</NavLink>
+            </li>
+
+          </ul> 
+          <p className="styled-contact">  <SettingsPhoneIcon className='call-settings' />  +254 701 218 138</p>
       
+        </div>
+        </div>
+        </Modal> 
+        
+     
+      </div>
+       
     </nav>
     
     
