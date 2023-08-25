e77387f2edaab64e4563c258b13128e30c0576a9
Tess <tess362@gmail.com>
2023-08-25 20:30:17 +0300

    Updated the projects section

--- portfolio-app/src/Components/PageRoutes/Projects.jsx
+++ portfolio-app/src/Components/PageRoutes/Projects.jsx
@@ -1,23 +1,70 @@
 import "../Styles/Projects.css";
-import image from "../images/Kincaid.jpg";
+import projects from "../Hocs/Projects.jsx"; 
+import { useState } from "react";
+import {Link} from 'react-router-dom';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import OpenInBrowser from '@mui/icons-material/OpenInBrowser';
+
+
 
 
 const Projects = () => {
-  return (
-    <div><h1>Projects</h1>
-    <p className="text-center text-light style-text">THere are a few past sample projects I`ve worked on. Want to see more? Email me. <br /> 
-    Based on my Tech stack</p>
-    <div className="container projects">
-      
-    <img src={image}  alt='Kincaid' className="img"/>
-    <img src={image}  alt='Kincaid' className="img"/>
-    <img src={image}  alt='Kincaid' className="img"/>
-    <img src={image}  alt='Kincaid' className="img"/>
-    <img src={image}  alt='Kincaid' className="img"/>
-    <img src={image}  alt='Kincaid' className="img"/>
-    </div>
-    </div>
-  )
+    const [showModal, setShowModal] = useState(false);
+    const [showId, setShowId] = useState(0);
+
+    const MouseOverhandler = (id) => {
+        setShowModal(true);
+        setShowId(id);
+    }
+
+    const MouseLeavehandler = () => {
+        setShowModal(false);
+        setShowId(0);
+    }
+
+    return (
+        <div className="">
+             <div className="text-center">
+                <h1>My <b className="bold">Projects</b></h1>
+                 <p className="style-text">These are a few past sample projects I&apos;ve worked on. 
+                 </p>
+               </div>
+            <section >
+                <div className="projects-grid">
+                    {projects.map((project) => (
+                        <div
+                            key={project.id}
+                            onMouseOver={() => MouseOverhandler(project.id)}
+                            onMouseLeave={MouseLeavehandler}
+                            className="project-box"
+                            
+                        
+                        >
+                            <img alt={project.label} src={project.image}     className=" img-size " />
+                            {showModal && showId === project.id && (
+                                <div  className="modal-contentx" style={{ display: 'block' }}>
+                                    <p style={{ color: '#ff8f00', fontSize: '20px' }} >{project.label}</p>
+                                    <p style={{ color: '#27AE60', fontSize: '16px' }}>
+                                        Tech: <b style={{ color: '#e3f2fd' }} className="text-dark">{project.tech}</b>
+                                    </p>
+                                    <div>
+                                        <Link to={project.github} target="_blank" style={{ color: '#e3f2fd', fontSize: '18px' }} className="text-dark"> 
+                                             Github < GitHubIcon  className="text-dark"/>
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link to={project.server} target="_blank" style={{ color: '#e3f2fd', fontSize: '18px' }}  className="text-dark">
+                                           Click to View <OpenInBrowser className="text-dark" />
+                                        </Link>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </div>
+    );
 }
 
-export default Projects
+export default Projects;
