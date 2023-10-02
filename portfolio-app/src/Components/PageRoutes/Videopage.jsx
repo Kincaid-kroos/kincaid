9388e078e27c4a48450528853653264e570a8531
kincaid <kiprotichkincaid@gmail.com>
2023-10-02 14:21:40 +0300

    made the videos responsive

--- portfolio-app/src/Components/PageRoutes/Videopage.jsx
+++ portfolio-app/src/Components/PageRoutes/Videopage.jsx
@@ -1,6 +1,7 @@
-
 import projects from "../Hocs/Projects.jsx"; 
 import { useParams } from 'react-router-dom';
+import '../Styles/Videopage.css';
+
 
 const Videopage = () => {
   const { projectId } = useParams();
@@ -11,10 +12,12 @@ const Videopage = () => {
   return (
     <div className="container mt-10 text-white">
       <h2>{project.label}</h2>
-      <video controls  className="video">
-        <source src={project.video} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div className="video-container">
+        <video controls className="video">
+          <source src={project.video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <p>{project.describtion}</p>
     </div>
   );
